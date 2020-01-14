632edf6fcd9d7948ed266a4c0e74d2ac823cafbd
sshregina <sheila.regina.94@gmail.com>
2020-01-14 14:36:08 -0300

    firebase deploy

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Burger Queen
+
+## 1. Aplicação
+
+
+<!-- This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 
@@ -65,4 +70,4 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 
 ### `yarn build` fails to minify
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify -->
--- src/App.js
+++ src/App.js
@@ -3,31 +3,6 @@ import "typeface-roboto";
 import Lounge from './view/pages/lounge/lounge'
 import { CssBaseline } from '@material-ui/core'
 
-// import firebaseImpl from './utils/firebase'
-// import { useEffect } from 'react'
-
-// import { getBurgers, getBeverages, getSideDish, getBreakfast, getLunch } from './infra/services/firebaseQueries';
-
-// getBurgers().then((burgers) => {
-//   console.log(burgers);
-// })
-
-// getBeverages().then((beverages) => {
-//   console.log(beverages);
-// })
-
-// getSideDish().then((sideDish) => {
-//   console.log(sideDish);
-// })
-
-// getBreakfast().then((breakfast) => {
-//   console.log(breakfast);
-// })
-
-// getLunch().then((lunch) => {
-//   console.log(lunch);
-// })
-
 function App() {
   return (
     <div>
